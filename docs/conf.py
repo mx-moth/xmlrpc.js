817323c8eded30d6184276f51d89d9502316eb35
Tim Heap <tim@ionata.com.au>
2013-04-19 17:24:55 +1000

    Remove more references to jQuery

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# jQuery XML-RPC documentation build configuration file, created by
+# xmlrpc.js documentation build configuration file, created by
 # sphinx-quickstart on Mon Oct 22 19:51:25 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'jQuery XML-RPC'
+project = u'xmlrpc.js'
 copyright = u'2012, Tim Heap'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'jQueryXML-RPCdoc'
+htmlhelp_basename = 'xmlrpc-jsdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'jQueryXML-RPC.tex', u'jQuery XML-RPC Documentation',
+  ('index', 'xmlrpc-js.doc', u'xmlrpc.js Documentation',
    u'Tim Heap', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'jqueryxml-rpc', u'jQuery XML-RPC Documentation',
+    ('index', 'xmlrpc-js', u'xmlrpc.js Documentation',
      [u'Tim Heap'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'jQueryXML-RPC', u'jQuery XML-RPC Documentation',
-   u'Tim Heap', 'jQueryXML-RPC', 'One line description of project.',
+  ('index', 'xmlrpc-js', u'xmlrpc.js Documentation',
+   u'Tim Heap', 'xmlrpc.js', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- docs/make.bat
+++ docs/make.bat
@@ -99,9 +99,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\jQueryXML-RPC.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\xmlrpc-js.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\jQueryXML-RPC.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\xmlrpc-js.ghc
 	goto end
 )
 
